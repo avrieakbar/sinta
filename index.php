0a8ddf4a3d1d26033ebbe5a17ea3fd6ec579c7e4
avrieakbar <avrieap@gmail.com>
2022-01-15 10:06:36 +0700

    cobalagi

--- index.php
+++ index.php
@@ -5,14 +5,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>BUAT SINTA PACAR AVRIE</title>
+
+    <script>
+        module.exports = {
+  theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
+  }
+}
+    </script>
 </head>
 <body>
 <div style="text-align: center;">
-    <h1>Halooo Sinta pacar Avrie</h1>
-    <h2>Kata Avrie, Dia buat kesel Sinta yaa?</h2>
-    <a href="ya.html" class="btn btn-success">Iya :(</a>
-    <a href="tidak.html" class="btn btn-danger">Engga Kok :)</a>
+<h1 class="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600">
+  Hello, world!
+</h1>
 </div>
 </body>
 </html>
