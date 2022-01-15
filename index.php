8ddad8e2f7aa624adbeac45cc9d1cf4334d5f7e2
avrieakbar <avrieap@gmail.com>
2022-01-15 09:53:26 +0700

    coba

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>BUAT SINTA PACAR AVRIE</title>
+</head>
+<body>
+<div style="text-align: center;">
+    <h1>Halooo Sinta pacar Avrie</h1>
+    <h2>Kata Avrie, Dia buat kesel Sinta yaa?</h2>
+    <a href="ya.html" class="btn btn-success">Iya :(</a>
+    <a href="tidak.html" class="btn btn-danger">Engga Kok :)</a>
+</div>
+</body>
+</html>
